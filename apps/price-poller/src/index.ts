904857fcc88d9bb22800f9e430de6f886598747a
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2025-08-26 18:23:00 +0530

    add pubsub, getting stream data

--- apps/price-poller/src/index.ts
+++ apps/price-poller/src/index.ts
@@ -1,18 +1,20 @@
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+import { WebSocket, WebSocketServer } from "ws";
+import { pubSubManager } from "./utils/services";
 
-interface BinancePrice {
-  symbol: string;
-  price: string;
-}
+const wss = new WebSocket(
+  "wss://stream.binance.com:9443/stream?streams=btcusdt@bookTicker"
+);
 
-async function getBinancePrice(symbol: string): Promise<void> {
-  try {
-    const url = `https://api.binance.com/api/v3/ticker/price?symbol=${symbol}`;
-    const response = await axios.get<BinancePrice>(url);
-    console.log(`${symbol}: $${response.data.price}`);
-  } catch (error) {
-    console.error("Error fetching price:", error);
-  }
-}
+const main = async () => {
+  console.log("connecting ws");
+  wss.on("message", async (data) => {
+    const parsedData = JSON.parse(data.toString());
+    // console.log("data", parsedData);
+    await pubSubManager.publish("live_feed", parsedData);
+  });
+};
 
-// Exam
+main();
---
+++ packages/services/src/pubsub.ts
@@ -0,0 +1,50 @@
+import { createClient } from "redis";
+class PubSubManager {
+  publisher;
+  subscriber;
+  static instance: PubSubManager;
+
+  constructor(url: string) {
+    this.publisher = createClient({
+      url: "redis://default:WA9nGxg5rO2UR3GYCb8uwxx96zfrxV6w@redis-14029.c241.us-east-1-4.ec2.redns.redis-cloud.com:14029",
+    });
+    this.subscriber = createClient({
+      url: url,
+    });
+    this.publisher.on("error", (err: any) =>
+      console.log("Redis this.publisher Error", err)
+    );
+    this.subscriber.on("error", (err: any) =>
+      console.log("Redis subscriber Error", err)
+    );
+
+    this.connect();
+  }
+
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new PubSubManager(
+        "redis://default:WA9nGxg5rO2UR3GYCb8uwxx96zfrxV6w@redis-14029.c241.us-east-1-4.ec2.redns.redis-cloud.com:14029"
+      );
+    }
+    return this.instance;
+  }
+
+  async connect() {
+    await this.publisher.connect();
+    await this.subscriber.connect();
+  }
+
+  async publish(channel: string, message: any) {
+    // #todo update it, to accept both json and string
+    await this.publisher.publish(channel, JSON.stringify(message));
+  }
+  async subscribe(channel: string, cb: (msg: string) => void) {
+    await this.subscriber.subscribe(channel, (message) => {
+      console.log(message);
+      cb(message);
+    });
+  }
+}
+
+export const pubSubManager = PubSubManager.getInstance();
